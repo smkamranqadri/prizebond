ae88aa5333f604c03f936c2bde8010ecb5237c57
usama-ayub <usamaayub2012@gmail.com>
2017-05-04 08:24:28 -0700

    sql db set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,7 +37,7 @@ export class MyApp {
     this.platform.ready().then(() => {
       let db = new SQLite();
       db.openDatabase({
-        name: "data.db",
+        name: "hello.db",
         location: "default"
       }).then(() => {
         db.executeSql("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT)", {}).then((data) => {
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { Transfer } from 'ionic-native';
+import { NavController, Platform } from 'ionic-angular';
+import { Transfer, File, SQLite } from 'ionic-native';
 import { StoreBondNumber } from '../store-bond-number/store-bond-number';
 declare var window: any;
+declare var cordova: any;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,43 +11,80 @@ declare var window: any;
 export class HomePage {
   storeBondNumber: any;
   prizeBondCategory = [];
-  constructor(public navCtrl: NavController) {
+  public database: SQLite;
+  public people: Array<Object>;
+  constructor(public navCtrl: NavController, private platform: Platform) {
     this.prizeBondCategory = ['100', '200', '500', '750', '1500', '7500', '15000', '25000', '40000'];
     this.storeBondNumber = StoreBondNumber;
-  }
-  dbTo() {
-    window.plugins.sqlDB.copyDbToStorage('data.db', 1, '/sdcard/', true, this.success, this.error);
-  }
-  success(data) {
-    console.log("toData", data);
-  }
-  error(err) {
-    console.log("toError", err)
-  }
-
-  downloadFile() {
-    const fileTransfer = new Transfer();
-    const url = "https://drive.google.com/open?id=0B1BZyhMfg4s5dW84NWhGQkN4clk"
-
-    fileTransfer.download(url, '/sdcard/data.db/')
-      .then(data => {
-        console.log("data", data)
-        
-      }, err => {
-        console.log("Err", err)
+    this.platform.ready().then(() => {
+      this.database = new SQLite();
+      this.database.openDatabase({ name: "hello.db", location: "default" }).then(() => {
+        this.refresh();
+      }, (error) => {
+        console.log("ERROR: ", error);
       });
+    });
   }
-
-
-  dbFrom() {
-    window.plugins.sqlDB.copyDbFromStorage("data.db", 1, '/sdcard/data.db', true, this.fromSuccess, this.fromError);
+  public add() {
+    this.database.executeSql("INSERT INTO people (firstname, lastname) VALUES ('Nic', 'Raboy')", []).then((data) => {
+      console.log("INSERTED: " + JSON.stringify(data));
+    }, (error) => {
+      console.log("ERROR: " + JSON.stringify(error.err));
+    });
   }
-  fromSuccess(data) {
-    console.log("toData", data);
-    this.downloadFile();
+  public refresh() {
+    this.database.executeSql("SELECT * FROM people", []).then((data) => {
+      this.people = [];
+      if (data.rows.length > 0) {
+        for (var i = 0; i < data.rows.length; i++) {
+          this.people.push({ firstname: data.rows.item(i).firstname, lastname: data.rows.item(i).lastname });
+        }
+      }
+    }, (error) => {
+      console.log("ERROR: " + JSON.stringify(error));
+    });
   }
-  fromError(err) {
-    console.log("toError", err)
-  }
-
+   dbTo() {
+     window.plugins.sqlDB.copyDbToStorage('hello.db', 1, '/sdcard/myData', true, this.success, this.error);
+   }
+   success(data) {
+     console.log("toData", data);
+   }
+   error(err) {
+     console.log("toError", err)
+   }
+ 
+   downloadFile() {
+     const fileTransfer = new Transfer();
+     const url = "https://drive.google.com/open?id=0B1BZyhMfg4s5SnplRkVwRTRsS2c"
+ 
+     fileTransfer.download(url, '/sdcard/downloadDB.db')
+       .then(data => {
+         console.log("data", data)
+ 
+       }, err => {
+         console.log("Err", err)
+       });
+   }
+ 
+ 
+   dbFrom() {
+     // window.plugins.sqlDB.remove('re.db', 0, this.removeSuccess, this.removeError);
+     // window.plugins.sqlDB.copyDbFromStorage("data.db", 1, cordova.file.applicationStorageDirectory + 'data.db', true, this.fromSuccess, this.fromError);
+   }
+   removeSuccess(data) {
+     console.log("removeData", data);
+   }
+   removeError(err) {
+     console.log("removeError", err)
+   }
+/*   dbCopy() {
+     window.plugins.sqlDB.copy('re.db', 0, this.copySuccess, this.copyError);
+   }
+   copySuccess(data) {
+     console.log("copyData", data);
+   }
+   copyError(err) {
+     console.log("copyError", err)
+   }*/
 }
